5505537ef1a5b5a504b302e692201cd9597f3fa8
ArthurR2 <arturobreak@yahoo.com>
2022-02-18 11:04:54 -0500

    first commit

---
+++ 06_02/start/test/Form.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.junit.Assert.assertEquals;
+
+public class Form {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\BrowersDriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        FormPage formPage = new FormPage();
+
+
+        formPage.submitForm(driver);
+
+        waitForAlerBanner(driver);
+
+        assertEquals("The form was successfully submitted!", getAlertBannerText(driver));
+
+        driver.quit();
+    }
+
+
+    public static void waitForAlerBanner(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated((By.className("alert"))));
+    }
+
+    public static String getAlertBannerText(WebDriver driver){
+        return  driver.findElement(By.className("alert")).getText();
+    }
+
+}
